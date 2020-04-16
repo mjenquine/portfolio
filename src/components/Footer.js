ed0b1f4bdabff94c3d49d31b8daf4ae84cc706f0
Mark Jenquine <mjenquine@gmail.com>
2020-04-16 13:37:58 -0500

    removed icons from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,7 @@ class Footer extends Component {
       <footer className="footer">
         <div className="content has-text-centered ">
           <p>
-            <a href="https://www.linkedin.com/in/markjenquine/" target="_blank">
-              <i className="fab fa-linkedin fa-2x"></i>
-            </a>
-            <a href="https://github.com/mjenquine" target="_blank">
-              <i className="fab fa-github fa-2x"></i>
-            </a>
-            <a href="mailto:mjenquine@gmail.com" target="_blank" rel="noopener noreferrer">
-              <i className="fas fa-envelope-open-text fa-2x"></i>
-            </a>
+            
           </p>
         </div>
       </footer>
